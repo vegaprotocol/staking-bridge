c49ff61bd3efa6fdb227d78316d507bd40cc61dd
Jeremy Letang <me@jeremyletang.com>
2024-08-13 13:25:35 +0100

    chore: add test stake deposit
    Signed-off-by: Jeremy Letang <me@jeremyletang.com>

--- src/staking-bridge.sol
+++ src/staking-bridge.sol
@@ -7,65 +7,65 @@ import "./IStake.sol";
 /// @title ERC20 Staking Bridge
 /// @author Vega Protocol
 /// @notice This contract manages the vesting of the Vega V2 ERC20 token
-contract Vega_Staking_Bridge is IStake {
-  address _staking_token;
+contract StakingBridge is IStake {
+    address _stakingToken;
 
-  constructor(address token) {
-    _staking_token = token;
-  }
+    constructor(address token) {
+        _stakingToken = token;
+    }
 
-  /// @dev user => amount staked
-  mapping(address => mapping(bytes32 => uint256)) stakes;
+    /// @dev user => amount staked
+    mapping(address => mapping(bytes32 => uint256)) stakes;
 
-  /// @notice This stakes the given amount of tokens and credits them to the provided Vega public key
-  /// @param amount Token amount to stake
-  /// @param vega_public_key Target Vega public key to be credited with the stake
-  /// @dev Emits Stake_Deposited event
-  /// @dev User MUST run "approve" on token prior to running Stake
-  function stake(uint256 amount, bytes32 vega_public_key) public {
-    require(IERC20(_staking_token).transferFrom(msg.sender, address(this), amount));
-    stakes[msg.sender][vega_public_key] += amount;
-    emit Stake_Deposited(msg.sender, amount, vega_public_key);
-  }
+    /// @notice This stakes the given amount of tokens and credits them to the provided Vega public key
+    /// @param amount Token amount to stake
+    /// @param vegaPublicKey Target Vega public key to be credited with the stake
+    /// @dev Emits StakeDeposited event
+    /// @dev User MUST run "approve" on token prior to running Stake
+    function stake(uint256 amount, bytes32 vegaPublicKey) public {
+        require(IERC20(_stakingToken).transferFrom(msg.sender, address(this), amount));
+        stakes[msg.sender][vegaPublicKey] += amount;
+        emit StakeDeposited(msg.sender, amount, vegaPublicKey);
+    }
 
-  /// @notice This removes specified amount of stake of available to user
-  /// @dev Emits Stake_Removed event if successful
-  /// @param amount Amount of tokens to remove from staking
-  /// @param vega_public_key Target Vega public key from which to deduct stake
-  function remove_stake(uint256 amount, bytes32 vega_public_key) public {
-    stakes[msg.sender][vega_public_key] -= amount;
-    require(IERC20(_staking_token).transfer(msg.sender, amount));
-    emit Stake_Removed(msg.sender, amount, vega_public_key);
-  }
+    /// @notice This removes specified amount of stake of available to user
+    /// @dev Emits StakeRemoved event if successful
+    /// @param amount Amount of tokens to remove from staking
+    /// @param vegaPublicKey Target Vega public key from which to deduct stake
+    function removeStake(uint256 amount, bytes32 vegaPublicKey) public {
+        stakes[msg.sender][vegaPublicKey] -= amount;
+        require(IERC20(_stakingToken).transfer(msg.sender, amount));
+        emit StakeRemoved(msg.sender, amount, vegaPublicKey);
+    }
 
-  /// @notice This transfers all stake from the sender's address to the "new_address"
-  /// @dev Emits Stake_Transfered event if successful
-  /// @param amount Stake amount to transfer
-  /// @param new_address Target ETH address to recieve the stake
-  /// @param vega_public_key Target Vega public key to be credited with the transfer
-  function transfer_stake(uint256 amount, address new_address, bytes32 vega_public_key) public {
-    stakes[msg.sender][vega_public_key] -= amount;
-    stakes[new_address][vega_public_key] += amount;
-    emit Stake_Transferred(msg.sender, amount, new_address, vega_public_key);
-  }
+    /// @notice This transfers all stake from the sender's address to the "newAddress"
+    /// @dev Emits Stake_Transfered event if successful
+    /// @param amount Stake amount to transfer
+    /// @param newAddress Target ETH address to recieve the stake
+    /// @param vegaPublicKey Target Vega public key to be credited with the transfer
+    function transferStake(uint256 amount, address newAddress, bytes32 vegaPublicKey) public {
+        stakes[msg.sender][vegaPublicKey] -= amount;
+        stakes[newAddress][vegaPublicKey] += amount;
+        emit StakeTransferred(msg.sender, amount, newAddress, vegaPublicKey);
+    }
 
-  /// @dev This is IStake.staking_token
-  /// @return the address of the token that is able to be staked
-  function staking_token() external override view returns (address) {
-    return _staking_token;
-  }
+    /// @dev This is IStake.stakingToken
+    /// @return the address of the token that is able to be staked
+    function stakingToken() external view override returns (address) {
+        return _stakingToken;
+    }
 
-  /// @dev This is IStake.stake_balance
-  /// @param target Target address to check
-  /// @param vega_public_key Target vega public key to check
-  /// @return the number of tokens staked for that address->vega_public_key pair
-  function stake_balance(address target, bytes32 vega_public_key) external override view returns (uint256) {
-    return  stakes[target][vega_public_key];
-  }
+    /// @dev This is IStake.stakeBalance
+    /// @param target Target address to check
+    /// @param vegaPublicKey Target vega public key to check
+    /// @return the number of tokens staked for that address->vegaPublicKey pair
+    function stakeBalance(address target, bytes32 vegaPublicKey) external view override returns (uint256) {
+        return stakes[target][vegaPublicKey];
+    }
 
-  /// @dev This is IStake.total_staked
-  /// @return total tokens staked on contract
-  function total_staked() external override view returns (uint256) {
-    return IERC20(_staking_token).balanceOf(address(this));
-  }
+    /// @dev This is IStake.totalStaked
+    /// @return total tokens staked on contract
+    function totalStaked() external view override returns (uint256) {
+        return IERC20(_stakingToken).balanceOf(address(this));
+    }
 }
--- test/staking-bridge.t.sol
+++ test/staking-bridge.t.sol
@@ -2,19 +2,68 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
-// import {Counter} from "../src/Counter.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {StakingBridge} from "../src/staking-bridge.sol";
 
-contract CounterTest is Test {
-    //     Counter public counter;
+contract TestERC20 is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
 
-    function setUp() public {
-        /* counter = new Counter(); */
-        /* counter.setNumber(0); */
+    function mint(address recipient, uint256 amount) external {
+        _mint(recipient, amount);
     }
 
-    /* function test_Increment() public { */
-    /*     counter.increment(); */
-    /*     assertEq(counter.number(), 1); */
+    function burn(address owner, uint256 amount) external {
+        _burn(owner, amount);
+    }
+}
+
+contract StakingBridgeTest is Test {
+    event StakeDeposited(address indexed user, uint256 amount, bytes32 indexed vegaPublicKey);
+
+    TestERC20 public stakingToken;
+    StakingBridge public bridge;
+
+    function setUp() public {
+        stakingToken = new TestERC20("Staking", "STAKE");
+        bridge = new StakingBridge(address(stakingToken));
+    }
+
+    // Staking Bridge accepts and locks deposited VEGA
+    // tokens and emits Stake_Deposited event (0071-STAK-001)
+    function test_LocksTokenAndEmitEvent() public {
+        bytes32 pkey = 0x17a33504a3f676fe940d629da5105402df8c4b8d9d2665c02ed280abb0aa4278;
+	address user = address(1337);
+
+        assertEq(bridge.totalStaked(), 0);
+
+        // mint token to address 1337
+        stakingToken.mint(user, 10);
+        assertEq(stakingToken.balanceOf(user), 10);
+
+        // address 1337 approve bridge transfers
+        vm.prank(user);
+        stakingToken.approve(address(bridge), 10);
+
+        // address 1337 deposit tokens on the staking bridge
+        vm.prank(user);
+
+        // check emitted event
+        vm.expectEmit(true, true, true, false);
+        // The event we expect
+        emit StakeDeposited(user, 10, pkey);
+
+        bridge.stake(10, pkey);
+
+        // ensure balances
+        assertEq(bridge.totalStaked(), 10);
+        assertEq(bridge.stakeBalance(user, pkey), 10);
+        assertEq(stakingToken.balanceOf(user), 0);
+    }
+
+    /* function test_RemoveTokenAndEmitEvent() public { */
+    /*     bytes32 pkey = 0x17a33504a3f676fe940d629da5105402df8c4b8d9d2665c02ed280abb0aa4278; */
+    /* 	address user = address(1337); */
+    /*     assertEq(bridge.totalStaked(), 0); */
     /* } */
 
     /* function testFuzz_SetNumber(uint256 x) public { */
